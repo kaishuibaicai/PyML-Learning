2fca975a9f497aa8e6d9d508a84a1093f9fb505e
徐梓琅 <272251416@qq.com>
2017-12-05 09:49:49 +0800

    add new figure

--- a_FirstExample/Regression.py
+++ a_FirstExample/Regression.py
@@ -13,6 +13,30 @@ x, y = np.array(x), np.array(y)
 
 x = (x - x.mean()) / x.std()
 
-plt.figure()
-plt.scatter(x, y, c='g', s=6)
+# plt.figure()
+# plt.scatter(x, y, c='g', s=6)
+# plt.show()
+
+x0 = np.linspace(-2, 4, 100)
+def get_model(deg):
+	return lambda input_x=x0: np.polyval(np.polyfit(x, y, deg), input_x)
+
+
+def get_cost(deg, input_x, input_y):
+	return 0.5 * ((get_model(deg)(input_x) - input_y) ** 2).sum()
+
+
+test_set = (1, 4, 10)
+for d in test_set:
+	print(get_cost(d, x, y))
+
+
+plt.scatter(x, y, c="g", s=20)
+for d in test_set:
+	plt.plot(x0, get_model(d)(), label="degree = {}".format(d))
+
+plt.xlim(-2, 4)
+plt.ylim(1e5, 8e5)
+
+plt.legend()
 plt.show()
