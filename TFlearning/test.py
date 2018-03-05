52f140112bff3046e68a6bc197614cb8a3af7b33
徐梓琅 <272251416@qq.com>
2018-03-05 18:16:20 +0800

    betterdays

--- TFlearning/test.py
+++ TFlearning/test.py
@@ -1,2 +1,12 @@
 import pandas as pd
 print (pd.__version__)
+
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([94939, 39945,20239])
+
+pd.DataFrame({'City names': city_names, 'Population': population})
+
+
+california_housing_dataframe = pd.read_csv('C:\\Users\\Administrator\\Desktop\\PyML-Learning\\TFlearning\\california_housing_train.csv', sep=',')
+california_housing_dataframe.describe()
